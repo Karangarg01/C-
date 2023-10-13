8b25027c9e7515e2f9854fd9203709804f36c40d
Karan Garg <karan018522@gmail.com>
2023-10-13 23:55:30 +0530

    Create pointer.cpp

---
+++ DSA/pointer.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*int arr1[4] = {101,122,157,110};
+    int arr1[4] = {24, 9, 10, 15};
+    int arr2[3] = {157,122,97};
+    int arr2[3] = {24, 8, 9};
+    int *ptr1 = arr1;
+    int *ptr2 = arr2;
+
+    cout <<*ptr1 << endl;
+    cout <<*(ptr2+0) << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (*(ptr2+i) == *(ptr1+j))
+            {
+                cout << *(ptr2+i) << " ";
+                break;
+            }
+        }
+    }*/
+    return 0;
+}
