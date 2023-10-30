22492a37544f83f95bc59aedbd26026723a450f6
Karan Garg <karan018522@gmail.com>
2023-10-30 23:16:55 +0530

    Update intro.cpp

--- DSA/TREE/intro.cpp
+++ DSA/TREE/intro.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int value;
-    Node* left;
-    Node* right;
-    Node(int data){
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
         value = data;
         left = NULL;
         right = NULL;
     }
 };
 
-Node* create(Node* root){
+Node *create(Node *root)
+{
     cout << "Enter data: " << endl;
     int data;
     cin >> data;
     root = new Node(data);
-    if(data == -1){
+    if (data == -1)
+    {
         return NULL;
     }
 
@@ -28,8 +33,79 @@ Node* create(Node* root){
     root->right = create(root->right);
     return root;
 }
-int main(){
-    Node* root = NULL;
+
+void levelOrderTraversal(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+
+        else
+        {
+            cout << temp->value << " ";
+
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+void inorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->value << " ";
+    inorder(root->right);
+}
+
+void preorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->value << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->value << " ";
+}
+int main()
+{
+    Node *root = NULL;
     root = create(root);
+    postorder(root);
     return 0;
 }
