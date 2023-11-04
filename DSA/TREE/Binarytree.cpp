006d88a936976639b055fb2112ef8c044c82ccee
Karan Garg <karan018522@gmail.com>
2023-11-04 23:40:30 +0530

    Update Binarytree.cpp

--- DSA/TREE/Binarytree.cpp
+++ DSA/TREE/Binarytree.cpp
@@ -16,27 +16,33 @@ public:
     }
 };
 
-Node* insertIntoBST(Node* root, int d){
-    if(root==NULL){
+Node *insertIntoBST(Node *root, int d)
+{
+    if (root == NULL)
+    {
         root = new Node(d);
         return root;
     }
 
-    if(d>root->value){
-        root->right = insertIntoBST(root->right,d);
+    if (d > root->value)
+    {
+        root->right = insertIntoBST(root->right, d);
     }
 
-    else{
-        root->left = insertIntoBST(root->left,d);
+    else
+    {
+        root->left = insertIntoBST(root->left, d);
     }
     return root;
 }
 
-void takeINput(Node* root){
+void takeINput(Node *&root)
+{
     int data;
     cin >> data;
-    while(data!=-1){
-        root = insertIntoBST(root,data);
+    while (data != -1)
+    {
+        root = insertIntoBST(root, data);
         cin >> data;
     }
 }
@@ -109,11 +115,87 @@ void postorder(Node *root)
     postorder(root->right);
     cout << root->value << " ";
 }
-int main(){
-    Node* root = NULL;
+
+Node *min(Node *root)
+{
+    Node *temp = root->left;
+    while (temp->left != NULL)
+    {
+        temp = temp->left;
+    }
+    return temp;
+}
+
+Node *max(Node *root)
+{
+    Node *temp = root;
+    while (temp->right != NULL)
+    {
+        temp = temp->right;
+    }
+    return temp;
+}
+
+Node *deleteFromBST(Node *root, int val)
+{
+    if (root == NULL)
+    {
+        return root;
+    }
+    if (root->value == val)
+    {
+        // 0 child
+        if (root->left == NULL && root->right == NULL)
+        {
+            delete root;
+            return NULL;
+        }
+
+        // 1 child left
+        if (root->left != NULL && root->right == NULL)
+        {
+            Node *temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        // 1 child right
+        if (root->left == NULL && root->right != NULL)
+        {
+            Node *temp = root->right;
+            delete root;
+            return temp;
+        }
+
+        // 2 child
+        if (root->left != NULL && root->right != NULL)
+        {
+            int minval = min(root->right)->value;
+            root->value = minval;
+            root->right = deleteFromBST(root->right, minval);
+            return root;
+        }
+    }
+    else if (root->value > val)
+    {
+        root->left = deleteFromBST(root->left, val);
+        return root;
+    }
+    else
+    {
+        root->right = deleteFromBST(root->right, val);
+        return root;
+    }
+}
+int main()
+{
+    Node *root = NULL;
     cout << "Enter data to create BST" << endl;
     takeINput(root);
     cout << "Printting the BST" << endl;
     levelOrderTraversal(root);
-    
+    root = deleteFromBST(root,30);
+    cout << "Printting the BST" << endl;
+    levelOrderTraversal(root);
+
 }
