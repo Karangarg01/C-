759e427d3a989d9f5d3bb8001acdbd52fb113c6a
Karan Garg <121216986+Karangarg01@users.noreply.github.com>
2024-02-15 19:44:14 +0530

    Update Subsets.cpp

--- DSA/Array/Subsets.cpp
+++ DSA/Array/Subsets.cpp
@@ -1,3 +1,4 @@
+// Subsets in an Array
 class Solution {
 public:
 
@@ -21,3 +22,39 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+// Subsequence in a String
+#include <bits/stdc++.h> 
+
+void solve(string str, string output, int index, vector<string> &ans){
+	if(index>=str.length()){
+		if(output.length()>0){
+			ans.push_back(output);
+		}
+		return;
+	}
+
+	//exclude
+	solve(str, output, index+1, ans);
+
+	//include
+	char element = str[index];
+	output.push_back(element);
+	solve(str, output, index+1, ans);
+
+}
+vector<string> subsequences(string str){
+	
+	vector<string> ans;
+	string output= "";
+
+	int index=0;
+
+	solve(str, output, index, ans);
+	return ans;
+	
+}
