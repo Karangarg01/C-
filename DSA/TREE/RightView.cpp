2199bb8fd23bce0ae32f9148748470231e09815e
Karan Garg <121216986+Karangarg01@users.noreply.github.com>
2024-09-11 23:36:25 +0530

    Create RightView.cpp

---
+++ DSA/TREE/RightView.cpp
@@ -0,0 +1,34 @@
+vector<int> printRightView(BinaryTreeNode<int>* root) {
+    
+    vector<int> ans;
+
+    if(root==nullptr)
+        return ans;
+
+    queue<BinaryTreeNode<int>*> q;
+
+    q.push(root);
+
+    while(!q.empty()){
+
+        int size = q.size();
+
+        for(int i=0; i<size; i++){
+            BinaryTreeNode<int>* front = q.front();
+            q.pop();
+
+            if(i==size-1){
+                ans.push_back(front->data);
+            }
+
+            if(front->left)
+                q.push(front->left);
+
+            if(front->right)
+                q.push(front->right);
+            
+        }
+    }
+
+    return ans;
+}
